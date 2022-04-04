19a66bdfe68f8067bcb02aa935ca4f4edbbd35a9
Merbin J Anselm <merbin.j.anselm@sap.com>
2022-04-04 12:30:39 +0530

    skip list report page for my profile app

--- app/my-profile/webapp/Component.js
+++ app/my-profile/webapp/Component.js
@@ -1,3 +1,8 @@
-sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("profile.Component", {
-    metadata:{ manifest:'json' }
+sap.ui.define(["sap/fe/core/AppComponent"], AppComponent => AppComponent.extend("profile.Component", {
+    metadata:{ manifest:'json' },
+    init: function() {
+        var oHashChanger = sap.ushell.Container.getService("ShellNavigation").hashChanger;
+        oHashChanger.replaceHash('Employee(ID=236a30f4-0504-49fc-92db-c1719ce05bc4,IsActiveEntity=true)'); 
+        AppComponent.prototype.init.apply(this, arguments);                     
+    }
 }))
